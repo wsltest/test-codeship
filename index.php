220ef978239b19bc399458889fda050715bef107
wslcodeapps <sysadminwsl@weblogssl.com>
2016-03-22 11:23:18 +0000

    New index

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>;
+echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>";
 phpinfo();
 ?>
