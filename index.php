f9cad0b7c1f98a3b8cd13489779a9977a83c407e
wslcodeapps <sysadminwsl@weblogssl.com>
2016-03-23 10:23:16 +0000

    Delete deploy dir with appspec

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $fecha->setTimestamp($fichero);
 #Main Print
 echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>\n";
 echo "
-<table border="1">
+<table border='1'>
 <tr>
 <td>El timestamp es</td><td>". $fichero ."</td></tr>
 <tr><td>El hostname es</td><td>". $hostname . "</td></tr>
---
+++ scripts/deltrash.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf /var/www/html/deploy
+rm -rf /var/www/html/tmp
+rm -rf /var/www/html/logs
