c1002e9f1af7725c94ea4ef84fa175d432d2365c
wslcodeapps <sysadminwsl@weblogssl.com>
2016-03-23 09:42:29 +0000

    Test timestamp

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>";
+
+$fichero = file_get_contents('./release.txt', true);
+$hostname = gethostname();
+#Main Print
+echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>\n";
+echo "<h2>El timestamp es: ". $fichero ."</h2>\n";
+echo "<h3>El hostname es: ". $hostname . "</h3>\n";
+
 phpinfo();
 ?>
