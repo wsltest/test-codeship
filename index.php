d5a0ac01525fac2ccd75682f825aec189a52dff5
wslcodeapps <sysadminwsl@weblogssl.com>
2016-03-23 09:53:47 +0000

    add date

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 $fichero = file_get_contents('./release.txt', true);
 $hostname = gethostname();
+$fecha = new DateTime();
+$fecha->setTimestamp($fichero);
 #Main Print
 echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>\n";
-echo "<h2>El timestamp es: ". $fichero ."</h2>\n";
-echo "<h3>El hostname es: ". $hostname . "</h3>\n";
-
+echo "<h2>El timestamp es:\t". $fichero ."</h2>\n";
+echo "<h3>El hostname es:\t". $hostname . "</h3>\n";
+echo "<h3>La fecha es:\t" . $fecha->format('U = Y-m-d H:i:s') . "</h3>\n";
 phpinfo();
 ?>
