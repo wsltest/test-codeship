4ac464f6d4438df501e631b34bed815906ff8f98
wslcodeapps <sysadminwsl@weblogssl.com>
2016-03-23 10:09:49 +0000

    Add table content

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ $fecha = new DateTime();
 $fecha->setTimestamp($fichero);
 #Main Print
 echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>\n";
-echo "<h2>El timestamp es:\t". $fichero ."</h2>\n";
-echo "<h3>El hostname es:\t". $hostname . "</h3>\n";
-echo "<h3>La fecha es:\t" . $fecha->format('U = Y-m-d H:i:s') . "</h3>\n";
+echo "
+<table border="1">
+<tr>
+<td>El timestamp es</td><td>". $fichero ."</td></tr>
+<tr><td>El hostname es</td><td>". $hostname . "</td></tr>
+<tr><td>La fecha es</td><td>" . $fecha->format('Y-m-d H:i:s') . "</td></tr>
+</table>";
 phpinfo();
 ?>
