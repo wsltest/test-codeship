746521d433397987d53a1603e4016a45e8bf2607
wslcodeapps <sysadminwsl@weblogssl.com>
2016-03-23 10:43:04 +0000

    fix php error

--- index.php
+++ index.php
@@ -5,14 +5,13 @@ $fichero = rtrim($fich);
 $hostname = gethostname();
 $fecha = new DateTime();
 $fecha->setTimestamp($fichero);
+
 #Main Print
 echo "<h1><center>The Server Name: " . $_SERVER['SERVER_NAME'] ." </center></h1>\n";
-echo "
-<center><table border='1 >
-<tr>
-<td>El timestamp es</td><td>". $fichero ."</td></tr>
-<tr><td>El hostname es</td><td>". $hostname . "</td></tr>
-<tr><td>La fecha es</td><td>" . $fecha->format('Y-m-d H:i:s') . "</td></tr>
-</table></center>";
+echo "<center><table border='1'>\n
+<tr><td>El timestamp es</td><td>". $fichero ."</td></tr>\n
+<tr><td>El hostname es</td><td>". $hostname . "</td></tr>\n
+<tr><td>La fecha es</td><td>" . $fecha->format('Y-m-d H:i:s') . "</td></tr>\n
+</table></center>\n";
 phpinfo();
 ?>
